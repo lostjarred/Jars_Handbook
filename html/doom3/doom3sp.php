bb93410dcdc0d8940799c6acb214a6f63bbb09e3
lostjarred <lostotaku@outlook.com>
2021-07-04 13:46:53 +1000

    add doom 3 source ports

---
+++ html/doom3/doom3sp.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../assets/css/foundation.css">
+        <!--end of foundation-->
+        <link rel="stylesheet" href="../../css/main.css">
+        <link rel="stylesheet" href="../../css/mainfont.css">
+        <link rel="stylesheet" href="../../css/amber.css">
+        <script src="../../cdn/jquery-3.6.0.js"></script>
+        <script src="../../javascript/jqfadein.js"></script>
+        <script src="../../javascript/jsclock.js"></script>
+        <title>Handbook - Doom 3\ Source Ports</title>
+    </head>
+    <body>
+        <div class="borderall">
+
+            <div class="grid-x grid-padding-x fadein">
+                <!--HEADER-->
+                <div class="large-12 cell">
+                    <?php include( '../header.html' ) ?>
+                </div>
+            </div>
+
+            <div class="grid-x grid-padding-x fadein bordertop borderbottom">
+                <div class="large-12 cell">
+                    <div class="grid-x grid-padding-x">
+                        <div class="grid-x grid-padding-x">
+
+                        <div class="large-3 cell borderright">
+                            <!--MENU-->
+                                <?php include '../menu.html' ?>
+                            </div>
+
+                            <div class="large-8 cell">
+                                <!--CONTENT-->
+                                <div class="grid-x grid-padding-x">
+                                    <!--Header for content-->
+                                    <div class="large-12 cell">
+                                        <h2>Doom 3 - Source Ports</h2>
+                                    </div>
+                                </div>
+                                <div class="grid-x grid-padding-x">
+                                    <!--timer decoration thing-->
+                                    <div class="large-2 cell">
+                                        <p>>>></p>
+                                    </div>
+                                    <div class="large-6 cell">
+                                        <!--blank for spacing-->
+                                    </div>
+                                    <div class="large-4 cell">
+                                        <p id="clockp">00:00 00.00.0000</p>
+                                    </div>
+                                </div>
+                                <div class="grid-x grid-padding-x">
+                                    <!--content-->
+                                    <div class="large-12 cell">
+                                        <h3>Game Source Ports</h3>
+                                        <h4>Normal</h4>
+                                        <a href="https://dhewm3.org/"> <p>dhewm3 Main Site</p> </a>
+                                        <a href="https://github.com/dhewm/dhewm3"> <p>dhewm3 Github</p> </a>
+                                        <h4>BFG Edition</h4>
+                                        <a href="https://www.moddb.com/mods/rbdoom-3-bfg"> <p>RBDOOM-3-BFG ModDB</p> </a>
+                                        <a href="https://github.com/RobertBeckebans/RBDOOM-3-BFG/"> <p>RBDOOM-3-BFG Github</p> </a>
+                                        <h3>Editors</h3>
+                                        <h4>Dark Radiant</h4>
+                                        <a href="https://www.darkradiant.net/"> <p>Dark Radiant Main Site</p> </a>
+                                        <a href="https://github.com/codereader/DarkRadiant"> <p>Dark Radiant Github</p> </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="grid-x grid-padding-x">
+                    <!--Footer-->
+                    <div class="large-12 cell">
+                    <?php include '../footer.html' ?>
+                    </div>
+            </div>
+
+        </div>
+        <!--foundation-->
+        <script src="assets/js/vendor.js"></script>
+        <script src="assets/js/foundation.js"></script>
+    </body>
+</html>
