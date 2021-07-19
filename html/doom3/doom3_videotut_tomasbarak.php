8a6eeddd505de5f54cf35fa104e2b0259ed95815
lostjarred <lostotaku@outlook.com>
2021-07-19 11:04:57 +1000

    Add Doom 3 Video Tutorial Page Tomas Barak

---
+++ html/doom3/doom3_videotut_tomasbarak.php
@@ -0,0 +1,124 @@
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/assets/css/foundation.css">
+        <!--end of foundation-->
+        <link rel="stylesheet" href="/css/main.css">
+        <link rel="stylesheet" href="/css/mainfont.css">
+        <link rel="stylesheet" href="/css/amber.css">
+        <script src="/cdn/jquery-3.6.0.js"></script>
+        <script src="/javascript/jqfadein.js"></script>
+        <script src="/javascript/jsclock.js"></script>
+        <title>Handbook - Doom 3\ Tutorial\ Video Tomas Barak</title>
+    </head>
+    <body>
+        <div class="borderall">
+
+            <div class="grid-x grid-padding-x fadein">
+                <!--HEADER-->
+                <div class="large-12 cell">
+                    <?php include( '../header.html' ) ?>
+                </div>
+            </div>
+
+            <div class="grid-x grid-padding-x fadein bordertop borderbottom">
+                <div class="large-12 cell">
+                    <div class="grid-x grid-padding-x">
+
+                        <!--MENU-->
+                        <?php include '../menu.html' ?>
+
+                        <div class="large-9 cell">
+                            <!--MAIN CONTENT DIV-->
+                            <div class="grid-x grid-padding-x">
+                                <!--Header for content-->
+                                <div class="large-12 cell">
+                                    <h2>Video Tutorials by Tomas Barak</h2>
+                                </div>
+                            </div>
+
+                            <div class="grid-x grid-padding-x">
+                                <!--timer decoration thing-->
+                                <div class="large-2 cell">
+                                    <p>>>></p>
+                                </div>
+
+                                <div class="large-6 cell">
+                                    <!--blank for spacing-->
+                                </div>
+
+                                <div class="large-4 cell">
+                                    <p id="clockp">00:00 00.00.0000</p>
+                                </div>
+                            </div>
+
+                            <div class="grid-x grid-padding-x bordertop">
+                                <div class="large-12 cell">
+                                    <p>>> Video Tutorials by Tomas Barak</p>
+                                </div>
+                            </div>
+
+                            <div class="grid-x grid-padding-x">
+                                <!--content-->
+                                <div class="large-12 cell">
+                                    <h4>Tomas Barak - Video Tutorials</h4>
+                                    <hr>
+                                    <a href="https://www.youtube.com/playlist?list=PLaaUDU_fsZZ7msV_RQjwuM5elVyJkcZ5K"> <h5>Main Playlist</h5> </a>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/pqDjm0pMZu4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/u_xc7oXKq-E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/W9DnJSWlQD4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/BEAcMuyzUm0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/a5Pj-5DYle8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/zMAyuMU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Xi9U72hLbx4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/j4WMfXOHrkM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/tbFP_2jRhMA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/z0yS85SdaFM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Q5rQ1FNuOiw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/06nOSdUyyTs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/RVIjn88lvMY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/5SKySPXS-Kc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/bcEAWTeMICQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/J4B_RlnX8CI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                    <hr>
+                                    <iframe width="560" height="315" src="https://www.youtube.com/embed/da1c4vqS30Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="grid-x grid-padding-x">
+                <!--Footer-->
+                <div class="large-12 cell">
+                    <?php include '../footer.html' ?>
+                </div>
+            </div>
+
+        </div>
+        <!--foundation-->
+        <script src="/assets/js/vendor.js"></script>
+        <script src="/assets/js/foundation.js"></script>
+    </body>
+</html>
